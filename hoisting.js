d2cecdb4d010c78842954a4001455478db31d705
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-01 19:42:42 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -5,7 +5,7 @@ console.log("I just met you...");
 console.log("and this is crazy..");
 console.log("but here's my number..");
 console.log("so call me");
-return lyric;	  
+return lyric;
 }
 
 function crazy() {
