bfce2865a609d4e8aae1b10508c85f5001488e6a
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-01 20:04:27 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -19,12 +19,9 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
+  var name = "Kristin";
   sayMy();
-
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
   }
 }
