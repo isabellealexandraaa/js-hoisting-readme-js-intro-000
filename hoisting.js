932eac40d7597c9935b6890e3c58fdea74b00930
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-01 19:42:39 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,12 @@
-var answer = 'maybe';
 
 function callMe() {
-  console.log(answer);
+var lyric = "maybe";
+console.log("I just met you...");
+console.log("and this is crazy..");
+console.log("but here's my number..");
+console.log("so call me");
+return lyric;	  
 }
-callMe();
 
 function crazy() {
   // fix the code in here:
