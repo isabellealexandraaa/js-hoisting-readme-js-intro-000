04d181fdd8cf5d17156d9238d54c0231c6fe765d
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-01 19:29:54 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,17 +1,11 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  console.log("maybe");
 }
 
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
